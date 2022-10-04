8314af1961715353477c0ce2e0371e2e3dace61c
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2022-10-04 11:16:17 +0200

    V2

--- app/src/main/java/com/example/acdat_pruebaandroid/MainActivity.java
+++ app/src/main/java/com/example/acdat_pruebaandroid/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.acdat_pruebaandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Se empieza a programar a partir de aquí
+
+        Button btnSaludar = findViewById(R.id.btnSaludar);
+
+        Log.i("info", "Funciona");
+
+    }
+
+    public void saludar(View view) {
+        findViewById(R.id.textView);
     }
 }
