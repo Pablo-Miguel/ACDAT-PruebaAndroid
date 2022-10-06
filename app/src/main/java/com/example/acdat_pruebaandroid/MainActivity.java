dc7eede48476c5cdbcc3b59681226a4ccf9d4a73
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2022-10-06 09:16:54 +0200

    V4

--- app/src/main/java/com/example/acdat_pruebaandroid/MainActivity.java
+++ app/src/main/java/com/example/acdat_pruebaandroid/MainActivity.java
@@ -8,8 +8,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static Integer cont = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,16 +19,47 @@ public class MainActivity extends AppCompatActivity {
 
         //Se empieza a programar a partir de aquí
 
-        Button btnSaludar = findViewById(R.id.btnSaludar);
+        Button btnSaludar2 = findViewById(R.id.btnSaludar2);
+        Button btnCont = findViewById(R.id.btnCont);
+
+        btnSaludar2.setOnClickListener(this);
+        btnCont.setOnClickListener(this);
 
         Log.i("info", "Funciona");
 
+        /*
+        TextView texto = new TextView(this);
+
+        texto.setText("Hello, Android");
+
+        setContentView(texto);
+        */
     }
 
-    public void onClick(View view) throws InterruptedException {
-        TextView txtSaludar = findViewById(R.id.txtSaludar);
-        txtSaludar.setText("Hola mundo");
-        Toast.makeText(this, "Este mensaje se autodestruirá en 4 segundos", Toast.LENGTH_LONG).show();
-        Thread.sleep(4000);
+    public void onClick(View view) {
+        if(view.getId() == R.id.btnSaludar){
+            TextView txtSaludar = findViewById(R.id.txtSaludar);
+            txtSaludar.setText("Hola mundo1");
+            Button btnSaludar = (Button) view;
+            btnSaludar.setText("Has saludado1");
+            btnSaludar.setEnabled(false);
+            //Toast.makeText(this, "Este mensaje se autodestruirá en 4 segundos", Toast.LENGTH_LONG).show();
+            //Thread.sleep(4000);
+        } else if(view.getId() == R.id.btnSaludar2){
+            TextView txtSaludar = findViewById(R.id.txtSaludar);
+            txtSaludar.setText("Hola mundo2");
+            Button btnSaludar = (Button) view;
+            btnSaludar.setText("Has saludado2");
+            btnSaludar.setEnabled(false);
+            //Toast.makeText(this, "Este mensaje se autodestruirá en 4 segundos", Toast.LENGTH_LONG).show();
+            //Thread.sleep(4000);
+        }
+        else {
+            Button btnCont = (Button) view;
+            cont++;
+            btnCont.setText("Pulsación: " + cont);
+
+        }
+
     }
 }
